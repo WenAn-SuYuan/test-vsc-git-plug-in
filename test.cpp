f1613da0a33f16f4c06233c819ee5bf19115bd5e
WenAn <zhangzimeng803525@126.com>
2024-08-19 18:10:05 +0800

    add a function to print "hello world" and add a cmakelist

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
+void print();
 int main()
 {
     cout << "test success" << endl;
     cout << 1 << endl;
-
+    print();
     return 0;
+}
+void print() {
+    cout << "it is ok" << endl;
 }
